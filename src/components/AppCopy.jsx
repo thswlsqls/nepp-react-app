4681b728f3594a1bc076fbb7f8ee7823f4435f55
eunbin son <thswlsqls@naver.com>
2021-11-22 20:29:03 +0900

    this is dev branch

--- src/components/AppCopy.jsx
+++ src/components/AppCopy.jsx
@@ -24,6 +24,7 @@ function App() {
     <>
       <input />
       <br />
+      this is dev branch
       <div style={style}>{name}</div>
       <div className={classList}>{name}</div>
       {name === '리액트' ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다.</h2>}
